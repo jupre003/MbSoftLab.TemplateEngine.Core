ed943b323b7083b36630032436ca1eda94948767
Justin <50733221+jupre003@users.noreply.github.com>
2025-12-05 22:37:16 +0000

    Aktualisiere Ziel-Framework auf net8.0 und aktualisiere Paketversionen; modernisierte Programmlogik auf TopLevelStatements

--- MbSoftlab.TemplateEngine.Core.Demo/Program.cs
+++ MbSoftlab.TemplateEngine.Core.Demo/Program.cs
@@ -1,62 +1,71 @@
 ﻿using MbSoftLab.TemplateEngine.Core;
+using MbSoftLab.TemplateEngine.Core.Demo;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
-namespace MbSoftLab.TemplateEngine.Core.Demo
+Person testModel = new Person
 {
-    class Program
+    FirstName = "Justin",
+    LastName = "LastName",
+    Tags = new List<string> { "Tag1", "Tag2", "Tag3" },
+    Address = new Address
     {
-        static void Main(string[] args)
+        Street = "Straße",
+        PostCode = "7872"
+    },
+    Orders = new List<Order>
+    {
+        new()
         {
-            Person testModel = new Person()
+            Id = 1,
+            Products = new List<Product>
             {
-                FirstName = "Justin",
-                LastName = "LastName",
-                Tags = new List<string>() { "Tag1", "Tag2", "Tag3" },
-                Address=new Address()
-                {
-                    Street="Straße",
-                    PostCode="7872"
-                },
-                Orders=new List<Order>()
-                {
-                    new Order()
-                    {
-                        Id=1,
-                        Products=new List<Product>()
-                        {
-                            new Product()
-                            {
-                               ProductName="Product1",
-                               Price=150
-                            },
-                            new Product()
-                            {
-                               ProductName="Product2",
-                               Price=50
-                            }
-                        } 
-                    }
-                }
-                
-            };
-
-
-
-            ITemplateEngine<Person> templateEngine = new RazorTemplateEngine<Person>();
-            templateEngine.LoadTemplateFromFile<Person>("TestModel.cshtml");
-            string templateFileContent = templateEngine.CreateStringFromTemplate(testModel);
-            Console.WriteLine();
-            Console.WriteLine(templateFileContent);
-
-            var htmlFileName = Path.Combine(Path.GetTempPath(), "temp.html");
-            File.WriteAllText(htmlFileName, templateFileContent);
-            Process.Start(@"cmd.exe ", $@"/c {htmlFileName}");
-            Console.ReadLine();
-
-            
+                new() { ProductName = "Product1", Price = 150 },
+                new() { ProductName = "Product2", Price = 50 }
+            }
         }
+    }
+};
+
+ITemplateEngine<Person> templateEngine = new RazorTemplateEngine<Person>();
+templateEngine.LoadTemplateFromFile<Person>("TestModel.cshtml");
+var templateFileContent = templateEngine.CreateStringFromTemplate(testModel);
+Console.WriteLine();
+Console.WriteLine(templateFileContent);
+
+var htmlFileName = Path.Combine(Path.GetTempPath(), "temp.html");
+await File.WriteAllTextAsync(htmlFileName, templateFileContent);
+OpenInDefaultBrowser(htmlFileName);
+
+void OpenInDefaultBrowser(string filePath)
+{
+    try
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = "cmd",
+                Arguments = $"/c start \"\" \"{filePath}\"",
+                CreateNoWindow = true
+            });
+        }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+        {
+            Process.Start("open", filePath);
+        }
+        else
+        {
+            Process.Start("xdg-open", filePath);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Konnte Browser nicht öffnen: {ex.Message}");
+        Console.WriteLine($"Pfad: {filePath}");
     }
 }
